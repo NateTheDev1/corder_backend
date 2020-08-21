603068e78a1c7261e6896129bbaa39e3e5081496
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-21 10:54:59 -0400

    Stripe route added

--- src/gql/typeDefs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
-var gql = require("apollo-server-express").gql;
-var typeDefs = gql(__makeTemplateObject(["\n  type Query {\n    products: [Product]\n    product(id: ID!): Product\n  }\n\n  type Product {\n    id: ID!\n    category: String!\n    title: String!\n    nutrition: Nutrition!\n    image: String!\n    price: Float!\n  }\n\n  type Nutrition {\n    id: ID\n    product_id: Int\n    calories: Int\n    fat: Int\n    cholesterol: Int\n    protein: Int\n    caffeine: Int\n  }\n"], ["\n  type Query {\n    products: [Product]\n    product(id: ID!): Product\n  }\n\n  type Product {\n    id: ID!\n    category: String!\n    title: String!\n    nutrition: Nutrition!\n    image: String!\n    price: Float!\n  }\n\n  type Nutrition {\n    id: ID\n    product_id: Int\n    calories: Int\n    fat: Int\n    cholesterol: Int\n    protein: Int\n    caffeine: Int\n  }\n"]));
-module.exports = { typeDefs: typeDefs };
--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,26 @@ server.get("/", (req: any, res: any) => {
   res.status(200).json({ message: "Welcome to the corder API" });
 });
 
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+server.post("/api/charge", async (req: any, res: any) => {
+  const { id, amount, title } = req.body;
+
+  try {
+    await stripe.paymentIntents.create({
+      amount,
+      description: title,
+      currency: "USD",
+      payment_method: id,
+      confirm: true,
+    });
+
+    return res.status(200).json({ confirmation: "1243hjbhf34" });
+  } catch (erorr) {
+    return res.status(400).json({ error: "Unable to make purchase" });
+  }
+});
+
 server.listen(PORT, () => {
   console.info(`🚀 Server is running in ${ENV} on port ${PORT}`);
 });
