c9b25724d37eea802727950f6b739c866b1515da
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-22 19:20:46 -0400

    New Nutrition and product seed

--- database/seeds/02-seed_products.js
+++ database/seeds/02-seed_products.js
@@ -29,6 +29,46 @@ exports.seed = function (knex) {
           price: 7.99,
           title: "Chocolate Chip Cookie Chip Creme Freeze",
         },
+        {
+          id: 3,
+          category: "Latte",
+          image:
+            "https://media.biggby.com/wp-content/uploads/2017/01/04194319/MochaCaramelHot.png",
+          price: 7.99,
+          title: "Black Forest Latte",
+        },
+        {
+          id: 4,
+          category: "Latte",
+          image:
+            "https://media.biggby.com/wp-content/uploads/2017/01/04194318/CaramelTop_g_h.png",
+          price: 6.99,
+          title: "Buttercup Latte",
+        },
+        {
+          id: 5,
+          category: "Latte",
+          image:
+            "https://media.biggby.com/wp-content/uploads/2017/01/04194318/MellowTop_g_h.png",
+          price: 6.99,
+          title: "Gingersnap Latte",
+        },
+        {
+          id: 6,
+          category: "Freeze",
+          image:
+            "https://media.biggby.com/wp-content/uploads/2017/01/04193913/StrawberryShortcake_CF.png",
+          price: 7.99,
+          title: "Strawberry Shortcake Creme Freeze",
+        },
+        {
+          id: 7,
+          category: "Freeze",
+          image:
+            "https://media.biggby.com/wp-content/uploads/2017/01/04194315/RedBullCF.png",
+          price: 7.99,
+          title: "Red Bull Creme Freeze",
+        },
       ]);
     });
 };
